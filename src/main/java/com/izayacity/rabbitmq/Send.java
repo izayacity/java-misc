964d5d4ff44f77d3b4290c391ed12c7783905be2
Francis Yang <izayacity@gmail.com>
2019-04-14 17:39:21 +0800

    rabbitmq setup

---
+++ src/main/java/com/izayacity/rabbitmq/Send.java
@@ -0,0 +1,24 @@
+package com.izayacity.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_HOST = "localhost";
+
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(QUEUE_HOST);
+
+		try (Connection connection = factory.newConnection();
+			 Channel channel = connection.createChannel()) {
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = "Hello World!";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+			System.out.println(" [x] Sent '" + message + "'");
+		}
+	}
+}
